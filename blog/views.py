9743b5f9ab5513c52f4d2a4b8deb3af6f449c252
Andi Fathul <35810127+andifathulms@users.noreply.github.com>
2021-11-18 23:01:34 +0700

    Menambah register dan database

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
+from .models import Post
 
 def home_view(request):
 	context = {}
+
+	post = Post.objects.all()
+	context["posts"] = post
 
 	return render(request,"home.html",context)
 
 def about_view(request):
 	context = {}
 
+	context["nama"] = "Fathul"
+	context["asal"] = "Makassar"
+	context["tinggi"] = "183"
+	context["bb"] = "85"
+	
 	return render(request,"about.html",context)
