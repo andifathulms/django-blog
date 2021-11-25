130a8280a60417cfc75cf6ae5a0bbcf42144a944
Andi Fathul <35810127+andifathulms@users.noreply.github.com>
2021-11-25 22:48:30 +0700

    menambahkan CRUD profile

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
 from django.urls import path, include
-
 from blog.views import home_view, about_view
-from users.views import register
+from users.views import register, profile
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('blog.urls')),
@@ -28,4 +30,8 @@ urlpatterns = [
     path('register/', register, name="register"),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
+    path('profile/', profile, name='profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
